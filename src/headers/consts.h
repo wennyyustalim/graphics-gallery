700b6d7eb9f6fce2b8cc27612d2a350f200f6b6d
Wenny Yustalim <wennyyustalim@gmail.com>
2018-03-03 14:51:07 +0700

    delete bin from git

--- src/headers/consts.h
+++ src/headers/consts.h
@@ -3,8 +3,8 @@
 #ifndef _APP_CONST_
 #define _APP_CONST_
 
-const int VIEWPORT_HEIGHT = 300;
-const int VIEWPORT_WIDTH = 300;
+const int VIEWPORT_HEIGHT = 500;
+const int VIEWPORT_WIDTH = 800;
 const int VIEWPORT_START_X = 0;
 const int VIEWPORT_START_Y = 0;
 const int CANVAS_HEIGHT= 600;
